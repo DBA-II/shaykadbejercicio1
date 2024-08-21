bde804972c8034190b2e80636c19bb62344c0ad6
shayka <shaykachow@gmail.com>
2024-08-21 15:35:37 -0600

    ejercicio1 creacion de base de datos

---
+++ Scripts/db1.sql
@@ -0,0 +1,7 @@
+ create database db1;
+ #bases de datos datos 
+create database morado;
+create database rosa;
+create database celeste;
+create database rojo;
+create database naranja;                                                                    
